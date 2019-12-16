a230d3d9060281daa1d4c75678f7c546d14a7190
Haifeng Mei <haifengmei@hotmail.com>
2019-12-16 14:12:31 -0400

    Removed api key for security

--- src/configs/firebaseConfig.js
+++ src/configs/firebaseConfig.js
@@ -1,10 +1,6 @@
+//Add personal firebase configuration
+
 var config = {
-    apiKey: "AIzaSyDLirqGN7R2bmx5963XLVTfLGHCIzZQVh8",
-    authDomain: "ezpay-c9127.firebaseapp.com",
-    databaseURL: "https://ezpay-c9127.firebaseio.com",
-    projectId: "ezpay-c9127",
-    storageBucket: "ezpay-c9127.appspot.com",
-    messagingSenderId: "433718443938"
 };
 
 export default config;
